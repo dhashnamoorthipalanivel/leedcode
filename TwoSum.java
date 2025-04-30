e1ebd4a17fba27912ade3ba49ba761f610028643
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-04-30 11:09:05 +0530

    Question no: 1 => Two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,21 @@
+package leedcode;
+
+public class TwoSum {
+    public class Solution{
+        public int[] twoSum(int [] numbers,int target){
+            int[] result = new int[2];
+            int sum = 0;
+            for(int i=0; i<numbers.length;i++){
+                for(int j=i+1; j<numbers.length;j++){
+                    sum = numbers[i]+numbers[j];
+                    if(sum == target){
+                         result[0] =i;
+                         result[1] =j;
+                         return result;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
