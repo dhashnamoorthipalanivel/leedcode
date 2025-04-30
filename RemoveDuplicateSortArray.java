c09588f4c40ad3a1d3651338cd84aa96f52db36b
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-04-30 11:39:26 +0530

    Question no: 26 => Remove duplicates from sorted array

---
+++ RemoveDuplicateSortArray.java
@@ -0,0 +1,19 @@
+package leedcode;
+
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicateSortArray {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            LinkedHashSet<Integer> set = new LinkedHashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int i = 0;
+            for (int val : set) {
+                nums[i++] = val;
+            }
+            return set.size();
+        }
+    }
+}
