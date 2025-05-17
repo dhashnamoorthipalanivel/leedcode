bd039dd8c1303763e6a6d84ab414e257422ab6db
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-05-17 13:56:19 +0530

    Search in Rotated Sorted Array

---
+++ SearchInArray.java
@@ -0,0 +1,12 @@
+// 33.Search in Rotated Sorted Array
+public class SearchInArray {
+
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
