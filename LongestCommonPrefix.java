e46255f32c7ee0cedf5afdeeadb4ffd984564d37
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-08-25 10:07:12 +0530

    Leedcode problem

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LongestCommonPrefix {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            Arrays.sort(strs);
+            String f = strs[0];
+            String l = strs[strs.length - 1];
+            int i = 0;
+            while (i < f.length() && i < l.length() && f.charAt(i) == l.charAt(i)) {
+                i++;
+            }
+            if (i == 0)
+                return "";
+            return f.substring(0, i);
+        }
+    }
+}
