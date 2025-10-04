c47a1da73a3b240c0ec3370c6a055974671de018
Dhashnamoorthi Palanivel <dhashnamoorthipalanivel@gmail.com>
2025-10-04 08:27:24 +0530

    Time: 2 ms (91.55%) | Memory: 42.4 MB (49.76%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,33 +1,32 @@
-import java.util.*;
-
 class Solution {
     public String removeDuplicateLetters(String s) {
-        HashMap<Character, Integer> m = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        boolean[] seen = new boolean[26];  
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            m.put(c, m.getOrDefault(c, 0) + 1);
+        boolean [] seen = new boolean [26];
+        int [] freq=new int[26];
+        for(int i =0; i<s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
+        Stack <Character> st = new Stack<>();
+        for(char c : s.toCharArray()) {
+            freq[c-'a']--;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            m.put(c, m.get(c) - 1); 
 
-            if (seen[c - 'a']) continue;
-
-            while (!st.isEmpty() && c < st.peek() && m.get(st.peek()) > 0) {
+            if(seen[c-'a']) continue;
+            while(!st.isEmpty() && c < st.peek() && freq[st.peek()-'a'] > 0 ) {
                 seen[st.pop() - 'a'] = false;
             }
 
+
             st.push(c);
-            seen[c - 'a'] = true;
+            seen[c-'a']=true;
         }
 
-        while (!st.isEmpty()) sb.insert(0, st.pop());
+        StringBuilder sb = new StringBuilder();
 
-        return sb.toString();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+
+        return sb.reverse().toString();
     }
 }
