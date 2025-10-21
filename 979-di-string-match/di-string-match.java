ebd12f9518c3d982fb1f7dfaaf398d438c3a731b
Dhashnamoorthi Palanivel <dhashnamoorthipalanivel@gmail.com>
2025-10-21 09:03:46 +0530

    Time: 3 ms (36.08%) | Memory: 44.9 MB (93.56%) - LeetSync

---
+++ 979-di-string-match/di-string-match.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int l = 0;
+        int h = s.length();
+        int[] arr = new int[h+1];
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == 'I'){
+                arr[i] = l;
+                l++;
+            }else{
+                arr[i] = h;
+                h--;
+            }
+        }
+        arr[arr.length-1] = l;
+        return arr;
+    }
+}
